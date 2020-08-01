77da411d29066d13a4476dc99afcbe0aafbf39a6
Diyar Faraj <diyar.faraj@gmail.com>
2020-08-02 00:00:44 +0200

    basic setup

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {UtilityThemeProvider, Box, Text} from 'react-native-design-utility';
+import {theme} from './src/constants/theme.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+
+  return(
+    <UtilityThemeProvider theme={theme}>
+      <Box f={1} center>
+        <Text>
+            helllo worlddd
+        </Text>
+      </Box>
+
+    </UtilityThemeProvider>
+
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
